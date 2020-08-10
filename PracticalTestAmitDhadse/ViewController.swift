af94dae715758a1a6e255fee4ee0d46142cebc3b
Amit Dhadse <amit.d.aux@go-jek.com>
2020-08-10 12:21:09 +0530

    Initial Commit

---
+++ PracticalTestAmitDhadse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticalTestAmitDhadse
+//
+//  Created by Amit Dhadse on 10/08/20.
+//  Copyright © 2020 Amit Dhadse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
